f440aeb6f23ee8613ffeade435e0264eb071bbbc
HenryVexZHou <43188301+NeverLucky123@users.noreply.github.com>
2020-03-15 12:54:11 -0700

    finalized effiency testing

--- main.cpp
+++ main.cpp
@@ -1,71 +1,106 @@
 #include <iostream>
 #include <memory>
-
+#include <any>
+#include <chrono>
 using namespace std;
-
+using namespace chrono;
 class DisVar {
 public:
-    unique_ptr<long long int> Ival;
-    unique_ptr<long double> Dval;
-    unique_ptr<string> Sval;
+    union valSet{
+        unique_ptr<long long int> Ival;
+        unique_ptr<long double> Dval;
+        unique_ptr<string> Sval;
+        ~valSet(){};
+        valSet() {};
+    }s;
+    DisVar(){}
     DisVar(long long int a) {
-        Ival=make_unique<long long int>(a);
+        s.Ival=make_unique<long long int>(a);
     }
     DisVar(long int a) {
-        Ival=make_unique<long long int>(a);
+        s.Ival=make_unique<long long int>(a);
     }
     DisVar(int a) {
-        Ival=make_unique<long long int>(a);
+        s.Ival=make_unique<long long int>(a);
     }
     DisVar(double a) {
-        Dval=make_unique<long double>(a);
+        s.Dval=make_unique<long double>(a);
     }
     DisVar(long double a) {
-        Dval=make_unique<long double>(a);
+        s.Dval=make_unique<long double>(a);
     }
 
     DisVar(string a) {
-        Sval=make_unique<string>(a);
+        s.Sval=make_unique<string>(a);
     }
-    auto operator+(const DisVar& a){
-
+    DisVar operator+(DisVar& a){
+        char b=this->type();
+        if(b!=a.type())
+            return DisVar();
+        else if(this->type())
+            
     }
     char type(){
-        if(Ival)
+        if(s.Ival)
             return 'i';
-        else if(Dval)
+        else if(s.Dval)
             return 'd';
-        else if(Sval)
+        else if(s.Sval)
             return 's';
     }
     string value(){
         char b=this->type();
         if(b=='i')
-            return to_string(*this->Ival);
+            return to_string(*this->s.Ival);
         if(b=='d')
-            return to_string(*this->Dval);
+            return to_string(*this->s.Dval);
         if(b=='s')
-            return *this->Sval;
+            return *this->s.Sval;
     }
+
 };
 string value(DisVar &a){
     char b=a.type();
     if(b=='i')
-        return to_string(*a.Ival);
+        return to_string(*a.s.Ival);
     if(b=='d')
-        return to_string(*a.Dval);
+        return to_string(*a.s.Dval);
     if(b=='s')
-        return *a.Sval;
+        return *a.s.Sval;
 }
+
 int main() {
+    //ptr class solution
     DisVar test=DisVar(16776879874578655);
     DisVar testd=DisVar(15.237896531246890);
     DisVar tests=DisVar("string");
     std::cout << sizeof(tests) << std::endl;
-    std::cout << test.value() << std::endl;
-    std::cout << value(testd) << std::endl;
-    std::cout << value(tests) << std::endl;
 
+    high_resolution_clock::time_point t1 = high_resolution_clock::now();
+    int d;
+    string e;
+    double s;
+    for(int c=1; c<1000000; c++) {
+        if(test.s.Ival)
+            d=*test.s.Ival;
+        else if(test.s.Dval)
+            s=*test.s.Dval;
+        else if(test.s.Sval)
+            e=*test.s.Sval;
+    }
+    high_resolution_clock::time_point t2 = high_resolution_clock::now();
+    duration time_span = duration_cast<duration<double>>(t2 - t1);
+    std::cout << time_span.count() << endl;
+
+    t1 = high_resolution_clock::now();
+    long long int fg;
+    for(int c=1; c<1000000; c++) {
+        fg=16776879874578655;
+    }
+    cout<<fg<<endl;
+    t2 = high_resolution_clock::now();
+    time_span = duration_cast<duration<double>>(t2 - t1);
+    std::cout << time_span.count() << endl;
 
     return 0;
 }
