d3d6526ad71b4f429f3bbde364be6b97e3d69aa4
HenryVexZHou <43188301+NeverLucky123@users.noreply.github.com>
2020-03-16 09:28:56 -0700

    a

--- main.cpp
+++ main.cpp
@@ -3,78 +3,83 @@
 #include <any>
 #include <chrono>
 #include <type_traits>
+
 using namespace std;
 using namespace chrono;
-class DisVar1 {
+
+class DisVar {
 private:
-    void* val{};
-    char type{};
+
 public:
-    inline DisVar1(){
-        val=nullptr;
+    void *val{};
+    char type{};
+    inline DisVar() {
+        val = nullptr;
     };
-    template <typename t>
-    inline explicit DisVar1(t a) {
-        val=new t(a);
-        if(typeid(a)==typeid(int))
-            type='i';
-        else if(typeid(a)==typeid(double))
-            type='d';
-        else if(typeid(a)==typeid(string))
-            type='s';
+    ~DisVar(){
+        free(val);
     }
-    DisVar1 operator+(DisVar1 a){
-        char b=this->type;
-        if(b!=a.type)
-            return DisVar1();
-        else{
-            switch (b){
+    template<typename t>
+    inline explicit DisVar(t a) {
+        val = new t(a);
+        if (typeid(a) == typeid(int))
+            type = 'i';
+        else if (typeid(a) == typeid(double))
+            type = 'd';
+        else if (typeid(a) == typeid(string))
+            type = 's';
+    }
+
+    inline DisVar operator+(const DisVar &a) {
+        if (this->type != a.type)
+            return DisVar();
+        else {
+            switch (this->type) {
                 case 'i':
-                    return DisVar1(*(int*)this->val+*(int*)(a.val));
+                    return DisVar(*static_cast<int*>(this->val) + *static_cast<int*>(a.val));
                 case 'd':
-                    return DisVar1(*(double*)this->val+*(double*)(a.val));
+                    return DisVar(*static_cast<double*>(this->val) + *static_cast<double*>(this->val));
                 case 's':
-                    return DisVar1(*(string*)this->val+*(string*)(a.val));
+                    return DisVar(*static_cast<string*>(this->val) + *static_cast<string*>(this->val));
+                default:
+                    return DisVar();
             }
         }
     }
-    inline string value(){
-        if(this->type=='i')
-            return to_string(*(int*)this->val);
-        else if(this->type=='d')
-            return to_string(*(double*)this->val);
-        else if(this->type=='s')
-            return *(string*)this->val;
+    inline string value() {
+        switch (this->type){
+            case 'i':
+                return to_string(*(int *)this->val);
+            case 'd':
+                return to_string(*(double *) this->val);
+            case 's':
+                return *(string *)this->val;
+            default:
+                return "error";
+        }
     }
 };
+
 int main() {
-    //ptr class solution
-    /*
-    DisVar test=DisVar(3467865);
-    DisVar test1(3467865);
-    DisVar testd=DisVar(15.237896531246890);
-    DisVar tests=DisVar("string");
-    std::cout << sizeof(tests) << std::endl;
-    */
 
     high_resolution_clock::time_point t1 = high_resolution_clock::now();
-    DisVar1 x;
-    DisVar1 test(245678);
-    DisVar1 test1(345675843);
-    for(int c=0; c<10000000; c++) {
-        x=test+test1;
+    DisVar x;
+    DisVar test(245678);
+    DisVar test1(345675843);
+    for (int c = 0; c < 10000000; c++) {
+        x = test + test1;
     }
     high_resolution_clock::time_point t2 = high_resolution_clock::now();
-    cout<<x.value()<<endl;
+    cout << x.value() << endl;
     duration time_span = duration_cast<duration<double>>(t2 - t1);
     std::cout << time_span.count() << endl;
 
     t1 = high_resolution_clock::now();
-    int fg;
-    for(int c=0; c<10000000; c++) {
-        fg=345675843+245678;
+    int fg = 0;
+    for (int c = 0; c < 10000000; c++) {
+        fg = 345675843 + 245678;
     }
-    cout<<fg<<endl;
+    cout << fg << endl;
     t2 = high_resolution_clock::now();
     time_span = duration_cast<duration<double>>(t2 - t1);
     std::cout << time_span.count() << endl;
