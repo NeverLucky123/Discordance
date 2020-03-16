29a033ef40c837cce66d3c4a8fb4a497214bbf69
HenryVexZHou <43188301+NeverLucky123@users.noreply.github.com>
2020-03-15 17:03:54 -0700

    unstable unique_ptr implementation

--- main.cpp
+++ main.cpp
@@ -11,33 +11,58 @@ public:
         unique_ptr<long double> Dval;
         unique_ptr<string> Sval;
         ~valSet(){};
-        valSet() {};
+        valSet(){};
+        valSet(valSet const &var){
+            if(var.Ival){
+                long long int i=*var.Ival;
+                Ival=make_unique<long long int>(i);
+            }
+            else if(var.Dval){
+                long double d=*var.Dval;
+                Dval=make_unique<long double>(d);
+            }
+            else if(var.Sval){
+                string s=*var.Sval;
+                Sval=make_unique<string>(s);
+            }
+        };
+        valSet& operator=(valSet const &var){};
     }s;
-    DisVar(){}
-    DisVar(long long int a) {
+    DisVar(){
+        s.Ival=nullptr;
+    }
+    explicit DisVar(long long int a) {
         s.Ival=make_unique<long long int>(a);
     }
-    DisVar(long int a) {
+    explicit DisVar(long int a) {
         s.Ival=make_unique<long long int>(a);
     }
-    DisVar(int a) {
+    explicit DisVar(int a) {
         s.Ival=make_unique<long long int>(a);
     }
-    DisVar(double a) {
+    explicit DisVar(double a) {
         s.Dval=make_unique<long double>(a);
     }
-    DisVar(long double a) {
+    explicit DisVar(long double a) {
         s.Dval=make_unique<long double>(a);
     }
 
-    DisVar(string a) {
+    explicit DisVar(string a) {
         s.Sval=make_unique<string>(a);
     }
-    DisVar operator+(DisVar& a){
+    DisVar operator+(DisVar a){
+      //  cout<<"adding"<<endl;
         char b=this->type();
         if(b!=a.type())
             return DisVar();
-        else if(this->type())
+        else{
+            if(this->s.Ival)
+                return DisVar(*this->s.Ival+*a.s.Ival);
+            else if(this->s.Dval)
+                return DisVar(*this->s.Dval+*a.s.Dval);
+            else if(this->s.Sval)
+                return DisVar(*this->s.Sval+*a.s.Sval);
+        }
             
     }
     char type(){
@@ -71,33 +96,29 @@ string value(DisVar &a){
 
 int main() {
     //ptr class solution
-    DisVar test=DisVar(16776879874578655);
+    DisVar test=DisVar(3467865);
+    DisVar test1(3467865);
     DisVar testd=DisVar(15.237896531246890);
-    DisVar tests=DisVar("string");
-    std::cout << sizeof(tests) << std::endl;
+   // DisVar tests=DisVar("string");
+   // std::cout << sizeof(tests) << std::endl;
+
 
     high_resolution_clock::time_point t1 = high_resolution_clock::now();
-    int d;
-    string e;
-    double s;
+    DisVar x;
     for(int c=1; c<1000000; c++) {
-        if(test.s.Ival)
-            d=*test.s.Ival;
-        else if(test.s.Dval)
-            s=*test.s.Dval;
-        else if(test.s.Sval)
-            e=*test.s.Sval;
+        x=test+test1;
     }
+    cout<<x.value()<<endl;
     high_resolution_clock::time_point t2 = high_resolution_clock::now();
     duration time_span = duration_cast<duration<double>>(t2 - t1);
     std::cout << time_span.count() << endl;
 
     t1 = high_resolution_clock::now();
-    long long int fg;
+    int *fg=new int;
     for(int c=1; c<1000000; c++) {
-        fg=16776879874578655;
+        *fg=3467865+3467865;
     }
-    cout<<fg<<endl;
+    cout<<*fg<<endl;
     t2 = high_resolution_clock::now();
     time_span = duration_cast<duration<double>>(t2 - t1);
     std::cout << time_span.count() << endl;
